6609a92876f85bafbb3212965be56d7651bf76e8
JCMaif <158159856+JCMaif@users.noreply.github.com>
2024-11-30 16:27:21 +0100

    first commit

---
+++ src/main/java/org/simplon/TrouveTonMatch/TrouveTonMatchApplication.java
@@ -0,0 +1,13 @@
+package org.simplon.TrouveTonMatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrouveTonMatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrouveTonMatchApplication.class, args);
+	}
+
+}
